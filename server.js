ece31955f48a046d29bf1a728fcf390e5b3e6d4c
zelstart <e.pomeroy95@gmail.com>
2023-09-11 22:00:41 -0400

    new notes are now saved to db

--- server.js
+++ server.js
@@ -26,47 +26,49 @@ app.get('/notes', (req, res) =>
 
 // wildcard route to 404 page
 app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/404.html'))
+  res.sendFile(path.join(__dirname, 'public/404.html'))
 );
 
 // post 
 app.post('/notes', (req, res) => {
-  // read existing notes from db.json
-  fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to read notes from db.json' });
-      return;
-    }
+  // log that a POST req was received
+  console.info(`${req.method} request received to add a new note to db.`)
 
-    // parse notes so we can push new note into it
-    let notes = JSON.parse(data);
+  // get title and text from the request body
+  const { title, text } = req.body;
 
-    // get title and text from the request body
-    const { title, text } = req.body;
-
+  // if note has both a title and text
+  if (title && text) {
     // create a new note object
     const newNote = {
       title,
       text,
-      // noteID (this is for deleting notes, if i decide to do it)
-    };
+    }
 
-    // add the new note to the array of notes
-    notes.push(newNote);
-
-    // write the updated notes array back to db.json
-    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
+    // read existing notes from db.json
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ error: 'Failed to write notes to db.json' });
-        return;
+      } else {
+        // parse notes so we can push new note into it
+        let notes = JSON.parse(data);
+
+        // add the new note to the array of notes
+        notes.push(newNote);
+
+        // write the updated notes array back to db.json
+        fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes, null, 2), (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to write notes to db.json' });
+            return;
+          }
+          console.log('A new note has been written to JSON file');
+          res.json(newNote); // respond with the newly created note
+        }); 
       }
-      console.log('A new note has been written to JSON file');
-      res.json(newNote); // respond with the newly created note
-    });
-  });
+    }); 
+  }
 });
 
-
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
